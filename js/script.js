f6cf9be6855da0abf25c846881cd115bdf719a6d
민 <nyon@kakao.com>
2025-05-02 01:11:46 +0900

    1

--- js/script.js
+++ js/script.js
@@ -1,148 +1,104 @@
 $(document).ready(function(){
 
-   let ww = $(window).width();
-   
-   if(ww<1000){
-    mobw();
-
-  }else{
-    pcw();
-  }
-
-  function mobw(){
-
-   $('.menu-btn').on('click',function(){
-      $('.mo-nav').animate({'margin-left':0})
-   })
-   $('.mo-nav ul li').on('click',function(){
-      $(this).addClass('on').siblings('li').removeClass('on')
-   })
-
-   $('.close-btn').on('click',function(){
-      $('.mo-nav').animate({'margin-left':'100%'})
-   })
-   
-   $(window).on('scroll',function(){
-      $('.h2-jail h2').delay(200).animate({'top':'-480px'},1200)
-      $('.menu-btn').delay(1100).animate({'height':'35px'},300).children('span').animate({'height':'7px'},300)
-   })
-
-      $('.top-btn').on('click',function(){
-         $('html, body').animate({'scrollTop':0},700)
-     })
-
-     var swiper = new Swiper(".sec4 .mySwiper", {
-      spaceBetween: 30,
-      slidesPerView : 1,
-      pagination: {
-        el: ".swiper-pagination",
-        dynamicBullets: true,
-
-      },
-    });
-
-    $('.mo-nav ul>li').on('click',function(){
-      let lieq = $(this).index()
-      let sectop = $('section').eq(lieq).offset().top;
-      $('html, body').stop().animate({'scrollTop':sectop});
-      $('.h2-jail h2').animate({'top':'-480px'},1200)
-      $('.menu-btn').animate({'height':'35px'},300).children('span').animate({'height':'7px'},300)
-      $('.mo-nav').delay(400).animate({'margin-left':'100%'})
-
-    })
-
-  }
-
-  function pcw(){
-
-   // $('.sec0 .map').on('mouseover',function(){
-   //    $(this).animate({'top':'0'})
-   //    $('.click-txt').animate({'marginTop':'10px','opacity':0},200)
-   // }).on('mouseout',function(){
-   //     $(this).stop().animate({'top':'-8%'})
-   //     $('.click-txt').animate({'marginTop':0,'opacity':1},250)
-   //  }).on('click',function(){
-   //     $('section').show()
-   //     $('.sec0').hide()
-   //  })
-
-   function abc(){
-      $('.click-txt span').animate({'marginRight':'5px'},500).animate({'marginRight':'15px'},500)
+    let ww = $(window).width();
+    
+    if(ww<1100){
+     mobw();
+ 
+   }else{
+     pcw();
    }
-   setInterval(abc,500)
+
+function mobw(){
+   
 
 
+}
 
-   $('section').on('mousewheel',function(e,d){
-      if(d<0){
-         //마우스휠을내렸을때일어나는일
-         let nxt = $(this).next().offset().top;
-         //내가선택한섹션 다음섹션의 top
-         $('html, body').stop().animate({'scrollTop':nxt});
-         //html,body를 대상으로 스크롤이 움직이기 nxt변수만큼
-      }else if(d>0){
-         //마우스휠을올렸을때일어나는일
-         let prv = $(this).prev().offset().top;
-         //내가선택한섹션 이전섹션의 top
-         $('html, body').stop().animate({'scrollTop':prv});
-         //html,body를 대상으로 스크롤이 움직이기 nxt변수만큼
-      }
+   
+function pcw(){
 
+    $('section').on('mousemove',function(e){
+        //마우스의 X축Y축위치값을 구함
+        let posX = e.pageX;
+        let posY = e.pageY;
 
-})
+        $('.bg-pattern').css({'top':-(posY/10),'left':-(posX/10)})
+    })
+    
+   $('.sec1').on('mousewheel',function(e,d){
+    if(d<0){
+       //마우스휠을내렸을때일어나는일
+    $(this).animate({'left':'-125%'},1100)
+    $(this).children('.sec-wrap').animate({'left':'125%'},1100)
+    $(this).children('.fruit').animate({'rotate':'-180deg','right':'100vw'},1100)
+    }})
 
-   var swiper = new Swiper(".sec4 .mySwiper", {
-         spaceBetween: 0,
-         slidesPerView : 3,
-         pagination: {
-         el: ".swiper-pagination",
-         dynamicBullets: true,
+    $('.sec2').on('mousewheel',function(e,d){
+        if(d<0){
+           //마우스휠을내렸을때일어나는일
+        $(this).animate({'left':'-125%'},1100)
+        $(this).children('.sec-wrap').animate({'left':'125%'},1100)
+        $(this).children('.fruit').animate({'rotate':'-180deg','right':'100vw'},1100)
+        }else if(d>0){
+           //마우스휠을올렸을때일어나는일
+        $(this).siblings('.sec1').animate({'left':0},1000)
+        $(this).siblings('.sec1').children('.sec-wrap').animate({'left':'0'},1000)
+        $(this).siblings('.sec1').children('.fruit').animate({'rotate':'180deg','right':'-25%'},1000)
+    }})
+    
+    $('.sec3').on('mousewheel',function(e,d){
+        if(d<0){
+           //마우스휠을내렸을때일어나는일
+        $(this).animate({'left':'-125%'},1100)
+        $(this).children('.sec-wrap').animate({'left':'125%'},1100)
+        $(this).children('.fruit').animate({'rotate':'-180deg','right':'100vw'},1100)
+        }else if(d>0){
+           //마우스휠을올렸을때일어나는일
+        $(this).siblings('.sec2').animate({'left':0},1000)
+        $(this).siblings('.sec2').children('.sec-wrap').animate({'left':'0'},1000)
+        $(this).siblings('.sec2').children('.fruit').animate({'rotate':'180deg','right':'-25%'},1000)
+    }})
+    
+        
+   //  $('.sec4').on('mousewheel',function(e,d){
+   //      if(d<0){
+   //         //마우스휠을내렸을때일어나는일
+   //      $(this).animate({'left':'-100%'},1000)
+   //      $(this).children('.sec-wrap').animate({'left':'125%'},1100)
+   //      $(this).children('.fruit').animate({'rotate':'-180deg','right':'75vw'},1000)
+   //      }else if(d>0){
+   //         //마우스휠을올렸을때일어나는일
+   //      $(this).siblings('.sec3').animate({'left':0},1000)
+   //      $(this).siblings('.sec3').children('.sec-wrap').animate({'left':'0'},1000)
+   //      $(this).siblings('.sec3').children('.fruit').animate({'rotate':'180deg','right':'-25%'},1000)
+   //  }})
 
-         },
-      });
+    $('.sec4').on('mousewheel',function(e,d){
+        if(d>0){
+           //마우스휠을올렸을때일어나는일
+        $(this).siblings('.sec3').animate({'left':0},1000)
+        $(this).siblings('.sec3').children('.sec-wrap').animate({'left':'0'},1000)
+        $(this).siblings('.sec3').children('.fruit').animate({'rotate':'180deg','right':'-25%'},1000)
+    }})
+// $('section').on('mousewheel',function(e,d){
+//     let sibleq = $(this).index()-1
+//     if(d<0){
+//        //마우스휠을내렸을때일어나는일
+//     $(this).animate({'width':0})
+//     }else if(d>0){
+//        //마우스휠을올렸을때일어나는일
+//     $(this).siblings('section').eq(sibleq).animate({'width':'100%'})
+//     }})
 
-$('.top-btn').on('click',function(){
-   $('html, body').animate({'scrollTop':0},700)
-})
+}//피씨끝
 
-   $('.map>li').on('click',function(){
-      let i = $(this).index();
-      let b = $('section').eq(i).offset().top;
-      //스크롤이 섹션의 offsettop값만큼 움직여야함
-      $('html, body').stop().animate({'scrollTop' : b})
-      // $(this).addClass('on').siblings('li').removeClass('on')
-   })
-
-
-         
-   $(window).on('scroll',function(){
-      let scr = $(window).scrollTop();
-      let top2 = $('.sec2').offset().top;
-      console.log(top2)
-      let top3 = $('.sec3').offset().top;
-      let top4 = $('.sec4').offset().top;
-      let top5 = $('.sec5').offset().top;
-      if(0<=scr && top2>scr){$('.map>li').eq(0).addClass('on').siblings().removeClass('on')
-          }else if(top2<=scr && top3>scr){$('.map>li').eq(1).addClass('on').siblings().removeClass('on')
-          }else if(top3<=scr && top4>scr){$('.map>li').eq(2).addClass('on').siblings().removeClass('on')
-          }else if(top4<=scr && top5>scr){$('.map>li').eq(3).addClass('on').siblings().removeClass('on')
-          }else{$('.map>li').eq(4).addClass('on').siblings().removeClass('on')
-      }
-          
-  })
-
-  }//이것이피씨끝
-
-  var delay = 300;
-  var timer = null; 
-  $(window).on('resize', function(){
-    clearTimeout(timer);
-    timer = setTimeout(function(){
-    document.location.reload();
-    }, delay);
-  });
-
-
-//   $(window).resize(function(){
-//    document.location.reload();})
+   lastWidth = window.innerWidth;
+    $(window).resize(function(){
+    if(window.innerWidth != lastWidth){
+    location.reload();
+    scrollTrigger.refresh();
+    }
+    lastWidth = window.innerWidth;
+    });
 })
